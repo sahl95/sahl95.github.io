b68dbc13de7e4582c6829d910a8d555e7fa77cfd
Sahl Rowther <sahl@macbook-pro.home>
2020-09-09 04:01:28 +0100

    Colour change

--- Videos/makeBetaComparisonVideo.sh
+++ Videos/makeBetaComparisonVideo.sh
@@ -6,6 +6,6 @@ ffmpeg -i b5500_stop.mp4 -vf drawtext="fontfile=/path/to/font.ttf:text='(b)': fo
 ffmpeg -i b15.mp4 -vf tpad=stop_mode=clone:stop_duration=5 b15_stop.mp4
 ffmpeg -i b15_stop.mp4 -vf drawtext="fontfile=/path/to/font.ttf:text='(a)': fontcolor=white: fontsize=24: box=1: boxcolor=black: x=10: y=15" -codec:a copy b15_label.mp4
 
-ffmpeg -i b15_label.mp4 -i b5500_label.mp4 -filter_complex "[0]pad=iw+50:color=#1C1D25[left];[left][1]hstack=inputs=2" betaComparison.mp4
+ffmpeg -i b15_label.mp4 -i b5500_label.mp4 -filter_complex "[0]pad=iw+50:color=#0B0D19[left];[left][1]hstack=inputs=2" betaComparison.mp4
 
 rm *_label.mp4 *_stop.mp4
