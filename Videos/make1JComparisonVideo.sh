8c76b89714c956639205338d500464267e3a4e0f
Sahl Rowther <sahl@macbook-pro.home>
2020-09-10 23:57:14 +0100

    planet migration videos

--- Videos/make1JComparisonVideo.sh
+++ Videos/make1JComparisonVideo.sh
@@ -1,4 +1,4 @@
-rm 1J_betaComparison.mp4 
+rm 1JbetaComparison.mp4 
 
 ffmpeg -i 1Jb5500.mp4 -vf tpad=stop_mode=clone:stop_duration=5 1Jb5500_stop.mp4
 ffmpeg -i 1Jb5500_stop.mp4 -vf drawtext="fontfile=/path/to/font.ttf:text='(b)': fontcolor=white: fontsize=24: box=1: boxcolor=black: x=10: y=15" -codec:a copy 1Jb5500_label.mp4
@@ -6,6 +6,6 @@ ffmpeg -i 1Jb5500_stop.mp4 -vf drawtext="fontfile=/path/to/font.ttf:text='(b)': 
 ffmpeg -i 1Jb15.mp4 -vf tpad=stop_mode=clone:stop_duration=5 1Jb15_stop.mp4
 ffmpeg -i 1Jb15_stop.mp4 -vf drawtext="fontfile=/path/to/font.ttf:text='(a)': fontcolor=white: fontsize=24: box=1: boxcolor=black: x=10: y=15" -codec:a copy 1Jb15_label.mp4
 
-ffmpeg -i 1Jb15_label.mp4 -i 1Jb5500_label.mp4 -filter_complex "[0]pad=iw+50:color=#0B0D19[left];[left][1]hstack=inputs=2" 1J_betaComparison.mp4
+ffmpeg -i 1Jb15_label.mp4 -i 1Jb5500_label.mp4 -filter_complex "[0]pad=iw+50:color=#0A0C1A[left];[left][1]hstack=inputs=2" 1JbetaComparison.mp4
 
 rm *_label.mp4 *_stop.mp4
